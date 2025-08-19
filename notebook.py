da08b61283a56123b26deace1bf990206fbf2f8c
Eric Hanson <ehanson@mit.edu>
2025-08-19 15:35:14 -0400

    Database search interface
    * Create a template for a database search interface using several UI elements

--- notebook.py
+++ notebook.py
@@ -12,6 +12,7 @@ __generated_with = "0.14.17"
 app = marimo.App(width="medium")
 
 
+# init
 @app.cell
 def _():
     import marimo as mo
@@ -23,39 +24,65 @@ def _():
 def _(mo):
     mo.md(
         r"""
-    # Hello World!
+    # Database Search
 
-    This notebook exercises launch via a Docker container.
+    This notebook searches a database.
     """
     )
     return
 
 
+# source data
 @app.cell
 def _(mo):
-    import sys
+    from tinydb import TinyDB
 
-    mo.md(f"""Python version: `{sys.version}`""")
-    return
+    db = TinyDB("db.json")
+
+    records = db.all()
+    fields = set()
+    for record in records:
+        fields.update(record.keys())
+    sorted_fields = sorted(fields)
+    return db, sorted_fields
 
 
+# query
 @app.cell
-def _(mo):
-    import tempfile
-    from tinydb import TinyDB, Query
-
-    with tempfile.TemporaryDirectory() as tmpdir:
-        db = TinyDB(f"{tmpdir}/db.json")
-        db.insert({"name": "test"})
-        results = db.all()
-
-    mo.md(
-        f"""
-    TinyDB loaded: `OK`<br>
-    Results: `{results}`
-    """
+def _(mo, sorted_fields):
+    search_string = mo.ui.text(
+        value="",
+        label="Search string",
     )
-    return
+    search_field = mo.ui.dropdown(options=sorted_fields, label="Search field")
+
+    run_button = mo.ui.run_button(label="Search")
+
+    mo.vstack([mo.md("### DB Search"), search_string, search_field, run_button])
+    return search_string, search_field, run_button
+
+
+@app.cell
+def _(mo, db, search_string, search_field, run_button):
+    from tinydb import Query
+
+    results = []
+    if run_button.value:
+        results = db.search(Query()[search_field.value] == search_string.value)
+    return results
+
+
+# render results
+@app.cell
+def _(mo, results):
+    table = mo.ui.table(data=results)
+    return table
+
+
+@app.cell
+def _(mo, table):
+    mo.vstack([table, table.value])
+    return table
 
 
 if __name__ == "__main__":
